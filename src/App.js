18cc0b1c3fc8af1fef77967611e3684a745c09b3
uddhav11 <uddhav.795421@cct.tu.edu.np>
2024-02-07 15:37:23 +0545

    Deploy portfolio site

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Aboutme from "./components/Aboutme";
+import Contacts from "./components/Contacts";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="text-gray-400 bg-gray-900 body-font">
+      {/* <h1>Hello, world!</h1> */}
+      <Navbar />
+      <Aboutme />
+      <Projects />
+      <Skills />
+      <Contacts />
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  content: ["./src/App.js", "./src/components/Navbar.jsx", "./src/components/Aboutme.jsx", "./src/components/Contacts.jsx", "./src/components/Projects.jsx", "./src/components/Skills.jsx"], // Include all JS/TS/JSX/TSX files in the src directory
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
